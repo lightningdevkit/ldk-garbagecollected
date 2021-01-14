bfbfeced77a2cf3ab06b33fe149666d6db225645
Arik Sosman <git@arik.io>
2021-01-14 03:17:14 -0800

    add typescript tuple generics

---
+++ lab/bindings/typescript/util.ts
@@ -0,0 +1,21 @@
+export class TwoTuple<A, B> {
+    public a: A;
+    public b: B;
+
+    constructor(a: A, b: B) {
+        this.a = a;
+        this.b = b;
+    }
+}
+
+export class ThreeTuple<A, B, C> {
+    public a: A;
+    public b: B;
+    public c: C;
+
+    constructor(a: A, b: B, c: C) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+}
