1a84658e8d67259bb115f610592d4ffa67790c8d
Matt Corallo <git@bluematt.me>
2021-06-12 21:47:48 +0000

    Drop _test_only_get_ptr as it is no longer used

--- java_strings.py
+++ java_strings.py
@@ -83,7 +83,6 @@ class CommonBase {
 	long ptr;
 	LinkedList<Object> ptrs_to = new LinkedList();
 	protected CommonBase(long ptr) { this.ptr = ptr; }
-	public long _test_only_get_ptr() { return this.ptr; }
 }
 """
 
