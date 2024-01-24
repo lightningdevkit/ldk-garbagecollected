644b85102d5bfffbfc409cdb871b8d0582240051
Matt Corallo <git@bluematt.me>
2024-01-24 03:17:10 +0000

    [C#] Free array buffers passed to C# after we've decoded them

--- c_sharp/src/org/ldk/util/InternalUtils.cs
+++ c_sharp/src/org/ldk/util/InternalUtils.cs
@@ -124,6 +124,7 @@ internal class InternalUtils {
 		byte[] res = new byte[len];
 		for (int i = 0; i < len; i++)
 			res[i] = getU8ArrayElem(arrptr, i);
+		bindings.free_buffer(arrptr);
 		return res;
 	}
 	public static short[] decodeUint16Array(long arrptr) {
@@ -131,6 +132,7 @@ internal class InternalUtils {
 		short[] res = new short[len];
 		for (int i = 0; i < len; i++)
 			res[i] = getU16ArrayElem(arrptr, i);
+		bindings.free_buffer(arrptr);
 		return res;
 	}
 	public static long[] decodeUint64Array(long arrptr) {
@@ -138,6 +140,7 @@ internal class InternalUtils {
 		long[] res = new long[len];
 		for (int i = 0; i < len; i++)
 			res[i] = getU64ArrayElem(arrptr, i);
+		bindings.free_buffer(arrptr);
 		return res;
 	}
 }
