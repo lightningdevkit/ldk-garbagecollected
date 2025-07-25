99421e384c78d518749056f629e38e8320b4fba1
Matt Corallo <git@bluematt.me>
2025-07-25 11:31:44 +0000

    [C#] Update auto-generated bindings to LDK 0.1.5.0 (w/ Testnet4)

--- c_sharp/src/org/ldk/enums/Network.cs
+++ c_sharp/src/org/ldk/enums/Network.cs
@@ -9,7 +9,11 @@ public enum Network {
 	/**
 	 * The testnet3 blockchain.
 	 */
-	LDKNetwork_Testnet,
+	LDKNetwork_Testnet3,
+	/**
+	 * The testnet4 blockchain.
+	 */
+	LDKNetwork_Testnet4,
 	/**
 	 * A local test blockchain.
 	 */
